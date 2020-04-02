f02f19ba809a51c363139bd26b87e34576c00ae2
kattyku01 <63068939+kattyku01@users.noreply.github.com>
2020-04-03 01:06:27 +0700

    Update claim.php

--- claim.php
+++ claim.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
-echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
+echo color("green","# # # # KattyKu # # # #\n");
 echo color("yellow","[•]  Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
-echo color("yellow","[•]              waiting proses.....           \n");
+echo color("yellow","[•]              By : Rina           \n");
 echo color("yellow","[•] penulisan nomor harus diawali 62xxxxxxxxxx \n");
 echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
 function change(){
